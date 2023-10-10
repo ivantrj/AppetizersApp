463047dcd81d5afe8202a449fc5a38c85874aa58
Ivan Trajanovski <ivantrajanovski@gmail.com>
2023-10-10 13:51:55 +0200

    create account ui

--- Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Screens/AccountView/AccountView.swift
@@ -8,10 +8,41 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthDate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+
+    
     var body: some View {
         NavigationView {
-            Text("Account")
-                .navigationTitle("😂 Account")
+            Form {
+                Section(header: Text("Personal Info")) {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.none)
+                    DatePicker("Birthday", selection: $birthDate, displayedComponents: .date)
+                    
+                    Button {
+                        print("Save")
+                    } label: {
+                        Text("Save Changes")
+                            .foregroundColor(Color.brandPrimary)
+                    }
+                }
+                
+                Section(header: Text("Requests")) {
+                    Toggle("Extra Napkings", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                }
+                .tint(Color.brandPrimary)
+
+            }
+            .navigationTitle("😂 Account")
         }
     }
 }
